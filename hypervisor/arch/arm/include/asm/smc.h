32ad46192eb89237eecddfbe6a758fe1af5c894a
Jie Deng <jiedeng@alumni.sjtu.edu.cn>
2024-05-10 10:35:56 +0800

    direct irq

--- hypervisor/arch/arm/include/asm/smc.h
+++ hypervisor/arch/arm/include/asm/smc.h
@@ -13,7 +13,7 @@
 /* for gcc < 5 */
 asm (".arch_extension sec\n");
 
-static inline int smc(unsigned long id)
+static inline long smc(unsigned long id)
 {
 	register unsigned long __id asm("r0") = id;
 
@@ -32,6 +32,20 @@ static inline int smc_arg1(unsigned long id, unsigned long par1)
 	asm volatile ("smc #0\n\t"
 		: "+r" (__id), "+r" (__par1)
 		: : "memory", "r2", "r3");
+
+	return __id;
+}
+
+static inline long smc_arg2(unsigned long id, unsigned long par1,
+			    unsigned long par2)
+{
+	register unsigned long __id asm("r0") = id;
+	register unsigned long __par1 asm("r1") = par1;
+	register unsigned long __par2 asm("r2") = par2;
+
+	asm volatile ("smc #0\n\t"
+		: "+r" (__id), "+r" (__par1), "+r" (__par2)
+		: : "memory", "r3");
 
 	return __id;
 }
